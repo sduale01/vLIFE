7ffe255ff4a714f5a99512069b857f67aa895708
Sharmarke Duale <sduale01@gmail.com>
2019-02-11 15:05:32 -0600

    create car registration info

---
+++ src/components/RegisterPage/RegisterCar.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+class RegisterCar extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <label htmlFor="make">
+                    Make:
+                    <input
+                        type="make"
+                        name="make"
+                        value={this.props.state.car_make}
+                        onChange={this.props.handleInputChangeFor('make')}
+                    />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="model">
+                    Model:
+                    <input
+                        type="model"
+                        name="model"
+                        value={this.props.state.car_model}
+                        onChange={this.props.handleInputChangeFor('model')}
+                    />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="year">
+                    Year:
+                    <input
+                        type="year"
+                        name="year"
+                        value={this.props.state.car_year}
+                        onChange={this.props.handleInputChangeFor('year')}
+                    />
+                    </label>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default RegisterCar;
