3add063bf7c920235012b0157ff91bbaf312c66e
Sharmarke Duale <sduale01@gmail.com>
2019-02-12 18:39:16 -0600

    reducer and saga files for sensor data

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,10 +2,13 @@ import { combineReducers } from 'redux';
 import errors from './errorsReducer';
 import loginMode from './loginModeReducer';
 import user from './userReducer';
+import sensor from './sensorReducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
+
+
 
 // Lets make a bigger object for our store, with the objects from our reducers.
 // This is what we get when we use 'state' inside of 'mapStateToProps'
@@ -13,6 +16,7 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
   user, // will have an id and username if someone is logged in
+  sensor, // contains reducers for sensors
 });
 
 export default rootReducer;
---
+++ src/redux/reducers/sensorReducer.js
@@ -0,0 +1,15 @@
+const sensorReducer = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_SENSOR':
+      // console.log(action.payload);
+      
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+  // user will be on the redux state at:
+  // state.user
+  export default sensorReducer;
+  
