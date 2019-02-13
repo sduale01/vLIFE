186eae6fd2b8a6a59caa355facc4e6e4e9ab8edb
Sharmarke Duale <sduale01@gmail.com>
2019-02-13 12:53:27 -0600

    server side put working, it will update sensor levels

--- server/routes/sensor.router.js
+++ server/routes/sensor.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const axios = require('axios');
 
-/**
- * GET route template
- */
+// add node cron
+const cron = require('node-cron');
+
 router.get('/', (req, res) => {
     // Send back user object from the session (previously queried from the database)
     const queryText =  `SELECT * FROM "sensors"`
@@ -15,7 +16,26 @@ router.get('/', (req, res) => {
         res.sendStatus(500);
     })
     // res.send(req.user);
-  });
+  }); // end of get
+
+  router.put('/update/:id', (req, res) => {
+    const queryText = `UPDATE "sensors" SET sensor_level = 72 where "id" = 4;`
+    pool.query(queryText).then(response => {
+        console.log('PUT is succesfull');
+        res.sendStatus(200);
+    }).catch(error => {
+        console.log('error in making PUT request', error);
+        res.sendStatus(500);
+    });
+    // axios.put('/api/sensor/update', queryText)
+    // .then(response => {
+    //     console.log(response.data);
+        
+    // }).catch(error => {
+    //     console.log('error in making PUT', error);
+    //     res.sendStatus(500);
+    // });
+  }); // end of put
 
 /**
  * POST route template
