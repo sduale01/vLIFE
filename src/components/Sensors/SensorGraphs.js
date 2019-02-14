84368fdf7d94c699edc38b1e059597c3ae0d0219
Sharmarke Duale <sduale01@gmail.com>
2019-02-14 14:51:12 -0600

    add sensor data auto refresh

--- src/components/Sensors/SensorGraphs.js
+++ src/components/Sensors/SensorGraphs.js
@@ -18,9 +18,25 @@ const options = {
   };
 
 class SensorGraphs extends Component {
-    componentDidMount() {
-        this.props.dispatch({type: 'FETCH_GAS_DATA'})
-    }
+  // GETS table data on page load
+  componentDidMount() {
+    this.props.dispatch({type: 'FETCH_GAS_DATA'})
+    this.startPolling();
+}
+// stops refresh when you leave page
+componentWillUnmount() {
+  clearInterval(this.interval);
+}
+// refreshes list every 2 seconds
+  startPolling() {
+    this.interval = setInterval(
+      () => { 
+        this.props.dispatch({type: 'FETCH_GAS_DATA'});
+      }, 2000);
+  }
+
+  
+
     render() {  
         return (
             <div>
