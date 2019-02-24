943ba7486b01c791f3df1465353509d3feb9b8a8
Sharmarke Duale <sduale01@gmail.com>
2019-02-24 15:15:50 -0600

    add homepage styling

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -19,6 +19,9 @@ const styles = theme => ({
       margin: theme.spacing.unit,
       height: '350px',
       width: '355px',
+      backgroundImage: 'linear-gradient(to bottom, #a37e93, #a6799e, #a376ad, #9876be, #8378d1, #7c79db, #737be5, #677def, #7e7bf1, #9379f2, #a876f2, #bb72f0)',
+      fontSize: 30,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
 
     },
     input: {
